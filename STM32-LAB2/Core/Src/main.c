d5d4351936806d48d78f3df48078f91c97f36e66
slowsly4r <khanh.vuongquoc@hcmut.edu.vn>
2025-10-01 09:56:30 +0700

    Ex9 proteus done

--- STM32-LAB2/Core/Src/main.c
+++ STM32-LAB2/Core/Src/main.c
@@ -57,13 +57,10 @@ void displayLEDMatrix(int num);
 void updateLEDMatrix(int index);
 void updateClockBuffer();
 void setSegTimer(int duration);
-void segTimer_run();
 void setDotTimer(int duration);
-void dotTimer_run();
 void setClockTimer(int duration);
-void clockTimer_run();
 void setMatrixTimer(int duration);
-void matrixTimer_run();
+void timer_run();
 void ledClear();
 void matrixClear();
 /* USER CODE END PFP */
@@ -318,10 +315,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	segTimer_run();
-	dotTimer_run();
-	clockTimer_run();
-	matrixTimer_run();
+	timer_run();
 }
 void display7SEG(int num) {
 		/*
@@ -435,37 +429,31 @@ void setSegTimer(int duration) {
 	segTimer_counter = duration / TIMER_CYCLE;
 	segTimer_flag = 0;
 }
-void segTimer_run() {
-	if (segTimer_counter > 0) {
-		segTimer_counter--;
-		if (segTimer_counter == 0) segTimer_flag = 1;
-	}
-}
 void setDotTimer(int duration) {
 	dotTimer_counter = duration / TIMER_CYCLE;
 	dotTimer_flag = 0;
-}
-void dotTimer_run() {
-	if (dotTimer_counter > 0) {
-		dotTimer_counter--;
-		if (dotTimer_counter == 0) dotTimer_flag = 1;
-	}
 }
 void setClockTimer(int duration){
 	clockTimer_counter = duration / TIMER_CYCLE;
 	clockTimer_flag = 0;
 }
-void clockTimer_run(){
-	if (clockTimer_counter > 0) {
-		clockTimer_counter--;
-		if (clockTimer_counter == 0) clockTimer_flag = 1;
-	}
-}
 void setMatrixTimer(int duration){
 	matrixTimer_counter = duration / TIMER_CYCLE;
 	matrixTimer_flag = 0;
 }
-void matrixTimer_run(){
+void timer_run(){
+	if (segTimer_counter > 0) {
+		segTimer_counter--;
+		if (segTimer_counter == 0) segTimer_flag = 1;
+	}
+	if (dotTimer_counter > 0) {
+		dotTimer_counter--;
+		if (dotTimer_counter == 0) dotTimer_flag = 1;
+	}
+	if (clockTimer_counter > 0) {
+		clockTimer_counter--;
+		if (clockTimer_counter == 0) clockTimer_flag = 1;
+	}
 	if (matrixTimer_counter > 0) {
 		matrixTimer_counter--;
 		if (matrixTimer_counter == 0) matrixTimer_flag = 1;
